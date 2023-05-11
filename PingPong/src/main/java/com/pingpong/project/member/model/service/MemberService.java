818f04c5ff32b728fcd570c632d8db90050df45f
nabong <99lnh@naver.com>
2023-05-11 21:15:40 +0900

    20230511_회원 가입구현 + 관리자 홈 버튼 생성

--- PingPong/src/main/java/com/pingpong/project/member/model/service/MemberService.java
+++ PingPong/src/main/java/com/pingpong/project/member/model/service/MemberService.java
@@ -4,6 +4,12 @@ import com.pingpong.project.member.model.dto.Member;
 
 public interface MemberService {
 
-   Member login(Member inputMember);
+	Member login(Member inputMember);
+
+	/** 회원가입 서비스
+	 * @param inputMember
+	 * @return result
+	 */
+	int signupInfo(Member inputMember);
 
 }
--- PingPong/src/main/java/com/pingpong/project/mypage/controller/MypageController.java
+++ PingPong/src/main/java/com/pingpong/project/mypage/controller/MypageController.java
@@ -12,4 +12,11 @@ public class MypageController {
 	public String personal() {
 		return "personal/post";
 	}
+	
+    // 내 정보 수정으로 이동
+    @GetMapping("/myPageModi")
+    public String myPageModi() {
+       return "personal/myPageModi"; 
+    }
+	   
 }
