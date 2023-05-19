0fc756a9503aec149ac292425cdc06042e8f493a
QLoooad <ckcc0813@gmail.com>
2023-05-19 12:13:21 +0900

    230519_닉네임 중복체크 완, 검색 진행중

---
+++ PingPong/src/main/java/com/pingpong/project/search/model/service/SearchServiceImpl.java
@@ -0,0 +1,16 @@
+package com.pingpong.project.search.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchServiceImpl implements SearchService{
+
+	@Override
+	public List<Map<String, Object>> headerSearch(String query) {
+		return null;
+	}
+
+}
