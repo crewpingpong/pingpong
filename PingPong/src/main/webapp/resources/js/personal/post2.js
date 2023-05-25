b6ddf2b01d37bc7cb8aed86971ebc5c5ffc6bf9b
nabong <99lnh@naver.com>
2023-05-25 16:40:27 +0900

    230525_나현 팔로우 팔로워 리스트 구현 유저 취소하는거 빼고 완

--- PingPong/src/main/webapp/resources/js/personal/post2.js
+++ PingPong/src/main/webapp/resources/js/personal/post2.js
@@ -1,21 +1,49 @@
 const followListBtn = document.querySelector(".followBtn>*");
-const myFollowList = document.querySelector(".myFollowList");
+const myFollowList = document.querySelector(".myFollowList"); // 팔로우
+const meFollowList = document.querySelector(".meFollowList"); // 팔로워
 if(followListBtn!=null){
-    followListBtn.addEventListener("click",()=>{
+    
+    const followList = document.querySelector(".followList");
+    followList.addEventListener("click",()=>{
         myFollowList.style.display = "flex";
         myFollowListFn();
     })
-    const followBoxX = document.querySelector(".follow-Box-X");
-    followBoxX.addEventListener("click",()=>{
-        myFollowList.style.display = "none";
-    })
-    const unfollow = document.querySelector(".unfollow");
-    if(unfollow!=null){
-        unfollow.addEventListener("click",()=>{
-            unfollowFn();
-        })
-    }
+
+    // const unfollow = document.querySelector(".unfollow");
+    // if(unfollow!=null){ // 언팔 기능
+    //     unfollow.addEventListener("click",()=>{
+    //         unfollowFn();
+    //     })
+    // }
 }
+
+const gotoFollower = document.querySelector(".goto-follower");
+if(gotoFollower!=null){
+    gotoFollower.addEventListener("click",()=>{
+        meFollowListFn();
+        myFollowList.style.display = "none"; // 팔로우 닫기
+        meFollowList.style.display = "flex"; // 팔로워 열기
+
+        const followerBoxX = document.querySelector(".follower-Box-X");
+        followerBoxX.addEventListener("click",()=>{ // 팔로워 닫기
+            meFollowList.style.display = "none";
+        })
+    })
+}
+const gotoFollow = document.querySelector(".goto-follow");
+if(gotoFollow!=null){
+    gotoFollow.addEventListener("click",()=>{
+        myFollowListFn();
+        myFollowList.style.display = "flex"; // 팔로우 열기
+        meFollowList.style.display = "none"; // 팔로워 닫기
+
+        const followBoxX = document.querySelector(".follow-Box-X");
+        followBoxX.addEventListener("click",()=>{ // 팔로우 닫기
+            myFollowList.style.display = "none";
+        })
+    })
+}
+
 
 
 function myFollowListFn(){ // 내가 팔로우하는 리스트 조회
@@ -26,7 +54,8 @@ function myFollowListFn(){ // 내가 팔로우하는 리스트 조회
         // 동적으로 생성할 요소를 담을 부모 요소를 선택합니다.
         const parentElement = document.querySelector('.myFollowList .followList');
         const myFollowCount = document.querySelector('.myFollowList .h4');
-        
+        myFollowCount.innerHTML="";
+
         const fmyFollowCountDiv = document.createElement('h4');
         fmyFollowCountDiv.textContent = "팔로우 ( "+ myFollowList.length +" )";
         myFollowCount.append(fmyFollowCountDiv);
@@ -89,13 +118,86 @@ function myFollowListFn(){ // 내가 팔로우하는 리스트 조회
     })
 }
 
-function unfollowFn(){
-    fetch("/alarm/")
-    .then(resp => resp.json())
-    .then(myFollowMap => {
 
+
+function meFollowListFn(){ // 내가 팔로우하는 리스트 조회
+    fetch("/alarm/meFollowList")
+    .then(resp => resp.json())
+    .then(meFollowList => {
+        // console.log(meFollowList.length);
+        // 동적으로 생성할 요소를 담을 부모 요소를 선택합니다.
+        const parentElement2 = document.querySelector('.meFollowList .followList');
+        const meFollowCount = document.querySelector('.meFollowList .h4');
+        meFollowCount.innerHTML="";
+        const fmyFollowCountDiv = document.createElement('h4');
+        fmyFollowCountDiv.textContent = "팔로워 ( "+ meFollowList.length +" )";
+        meFollowCount.append(fmyFollowCountDiv);
+
+        parentElement2.innerHTML="";
+        for(let mfl of meFollowList){
+
+        // 새로운 div 요소를 생성합니다.
+        const followExampleDiv = document.createElement('div');
+        followExampleDiv.classList.add('followExample');
+
+        // userProfile div 요소를 생성하고 내부 요소를 추가합니다.
+        const userProfileDiv = document.createElement('div');
+        userProfileDiv.classList.add('userProfile');
+
+        const profileLink = document.createElement('a');
+        profileLink.href = '/mypage/'+mfl.followerNo;
+
+        const profileBox = document.createElement('div');
+        profileBox.classList.add('probox');
+
+        const profileImage = document.createElement('img');
+        profileImage.src = mfl.followerImg;
+        // profileImage.alt = '';
+
+        profileBox.appendChild(profileImage);
+        profileLink.appendChild(profileBox);
+
+        const profileNameLink = document.createElement('a');
+        profileNameLink.href = '/mypage/'+mfl.followerNo;
+
+        const profileName = document.createElement('p');
+        profileName.textContent = mfl.followerName;
+
+        profileNameLink.appendChild(profileName);
+
+        userProfileDiv.appendChild(profileLink);
+        userProfileDiv.appendChild(profileNameLink);
+
+        // followManage div 요소를 생성하고 내부 요소를 추가합니다.
+        const followManageDiv = document.createElement('div');
+        followManageDiv.classList.add('followManage');
+
+        const unfollowButton = document.createElement('button');
+        unfollowButton.classList.add('unfollow');
+        unfollowButton.textContent = 'UNFOLLOW';
+
+        followManageDiv.appendChild(unfollowButton);
+
+        // 생성한 내부 요소들을 followExampleDiv에 추가합니다.
+        followExampleDiv.appendChild(userProfileDiv);
+        followExampleDiv.appendChild(followManageDiv);
+
+        // 부모 요소에 followExampleDiv를 추가합니다.
+        parentElement2.appendChild(followExampleDiv);    
+        }
     })
     .catch(err =>{
         console.log(err);
     })
 }
+
+// function unfollowFn(){
+//     fetch("/alarm/")
+//     .then(resp => resp.json())
+//     .then(myFollowMap => {
+        
+//     })
+//     .catch(err =>{
+//         console.log(err);
+//     })
+// }
